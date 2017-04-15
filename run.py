697f27aa9176ddf9fcb0fa0e95188bd6d5837f31
Inessa Vasilevskaya <inavasilevskaya@gmail.com>
2017-04-15 23:42:37 +0300

    orchids daemon: wait till influxdb can be pinged
    TBD: figure out how to use daemonize module

--- run.py
+++ run.py
@@ -1,5 +1,37 @@
 import daemon
 import fetch_data
+import logging
+import requests
+import sys
+import time
 
-with daemon.DaemonContext():
+import utils
+
+CONFIG = "config"
+TIMEOUT = 600
+STEP = 10
+LOG = logging.getLogger(__name__)
+
+
+def run():
+    params = utils.read_config(CONFIG, "default")
+    ping_url = "http://%(host)s:%(port)s/ping" % {"host": params.influx_host,
+                                                  "port": params.influx_port}
+    wait_c = 0
+    while wait_c < TIMEOUT:
+        # wait until influxdb accepts connections
+        try:
+            data = requests.get(ping_url)
+            if data.status_code == 204:
+                break
+        except requests.exceptions.ConnectionError:
+            time.sleep(STEP)
+            LOG.info("Influxdb unreachable, sleeping for 10 seconds..")
+            wait_c += STEP
+    if wait_c >= TIMEOUT:
+        LOG.error("Influxdb service was unreachable for %s seconds" % TIMEOUT)
+        sys.exit("Influxdb unreachable, exiting")
     fetch_data.main()
+
+
+run()
