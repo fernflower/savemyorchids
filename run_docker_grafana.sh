23ff512d51658226595ea2cd5ea46249ded30c43
Inessa Vasilevskaya <ivasilevskaya@mirantis.com>
2017-09-23 22:21:43 +0300

    docker: grafana/influx hosts can be configured

--- run_docker_grafana.sh
+++ run_docker_grafana.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ENV_FILE=config
+ENV_FILE=config.sample
 GRAFANA_SOURCE_NAME=orchids
 GRAFANA_DASHBOARD_FILE=grafana_dashboard.json
 GRAFANA_IMAGE=grafana/grafana
@@ -11,10 +11,11 @@ function get_val {
 }
 
 # XXX this sucks but will do for a poc
-influx_port=$(get_val "PORT")
-influx_user=$(get_val "USER")
-influx_pass=$(get_val "PASSWORD")
-influx_dbname=$(get_val "DBNAME")
+influx_port=$(get_val "INFLUX_PORT")
+influx_user=$(get_val "INFLUX_USER")
+influx_pass=$(get_val "INFLUX_PASSWORD")
+influx_dbname=$(get_val "INFLUX_DBNAME")
+influx_host=$(get_val "INFLUX_HOST")
 grafana_user=$(get_val "GRAFANA_AUTH_USER")
 grafana_pass=$(get_val "GRAFANA_AUTH_PASS")
 grafana_host=$(get_val "GRAFANA_HOST")
@@ -27,7 +28,7 @@ sudo docker run -d -p 3000:3000 --name grafana $GRAFANA_IMAGE
 sleep 3
 
 # create influxdb source in grafana
-curl "http://$grafana_user:$grafana_pass@$grafana_host:3000/api/datasources" -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary "{\"name\":\"$GRAFANA_SOURCE_NAME\",\"type\":\"influxdb\",\"url\":\"http://localhost:$influx_port\",\"access\":\"proxy\",\"isDefault\":true,\"database\":\"$influx_dbname\",\"user\":\"$influx_user\",\"password\":\"$influx_pass\"}"
+curl "http://$grafana_user:$grafana_pass@$grafana_host:3000/api/datasources" -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary "{\"name\":\"$GRAFANA_SOURCE_NAME\",\"type\":\"influxdb\",\"url\":\"http://$influx_host:$influx_port\",\"access\":\"proxy\",\"isDefault\":true,\"database\":\"$influx_dbname\",\"user\":\"$influx_user\",\"password\":\"$influx_pass\"}"
 
 # create a grafana dashboard from a json file
 curl "http://$grafana_user:$grafana_pass@$grafana_host:3000/api/dashboards/db" -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary "@$GRAFANA_DASHBOARD_FILE"
