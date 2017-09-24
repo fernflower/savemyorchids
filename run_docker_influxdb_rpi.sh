981e39ccea34d5e8539070976728bcf66b8872bf
Inessa Vasilevskaya <inavasilevskaya@gmail.com>
2017-09-25 01:42:06 +0300

    docker: influx_rpi has data volume

--- run_docker_influxdb_rpi.sh
+++ run_docker_influxdb_rpi.sh
@@ -15,4 +15,4 @@ influx_pass=$(get_val "PASSWORD")
 influx_dbname=$(get_val "DBNAME")
 
 # start influx container
-sudo docker run -d -p 8083:8083 -p "$influx_port:$influx_port" -e ADMIN_USER="$influx_user" -e INFLUXDB_INIT_PWD="$influx_pass" -e PRE_CREATE_DB="$influx_dbname" --expose 8090 --expose 8099 --name influxdb $INFLUX_IMAGE
+sudo docker run -d -p 8083:8083 -p "$influx_port:$influx_port" -e ADMIN_USER="$influx_user" -e INFLUXDB_INIT_PWD="$influx_pass" -e PRE_CREATE_DB="$influx_dbname" --volume /var/influxdb/:/data --expose 8090 --expose 8099 --name influxdb $INFLUX_IMAGE
